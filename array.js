a307de7b557c098c605366db28012c9b221f6954
shahrukh khan <shahrukhbadsah@gmail.com>
2022-01-04 23:11:50 +0530

    besic javascript

---
+++ array.js
@@ -0,0 +1,23 @@
+// An array can hold more than one value
+
+
+const cars = ['maruti', 'hyundai', 'honda', 'tata', 'mahindra', 'skoda', 'ford', 'mg']
+
+// array to string
+console.log(cars.toString(""))
+
+ // act like string but we can add any separator.
+console.log(cars.join("+"))
+
+//remove last element from array
+console.log(cars.pop())
+
+//suzuki will add in the array (end position)
+console.log(cars.push("suzuki"))
+
+console.log(cars.shift())
+
+console.log(cars)
+
+
+
